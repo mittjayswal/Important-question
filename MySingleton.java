66bde70cc6cbf5525cc330639947d7a5972b0253
mittjayswal <mittjayswal@gmail.com>
2018-02-11 21:46:48 -0800

    Singleton class example

---
+++ MySingleton.java
@@ -0,0 +1,27 @@
+package com.as.mitt.javainterview;
+
+public class MySingleton {
+
+	private static MySingleton obj;
+	
+	static {
+		obj = new MySingleton();
+	}
+	
+	private MySingleton() {
+		
+	}
+	
+	public static  MySingleton getInstance() {
+		return obj;
+	}
+	
+	public void test() {
+		System.out.println("Yes Its working");
+	}
+	public static void main(String[] args) {
+		MySingleton m = getInstance();
+		m.test();
+		
+	}
+}
