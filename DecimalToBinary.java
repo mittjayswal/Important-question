94962cf6538b73d506b6fcff2d1904a52c552cc5
mittjayswal <mittjayswal@gmail.com>
2018-02-11 21:46:15 -0800

    Decimal To binary

---
+++ DecimalToBinary.java
@@ -0,0 +1,23 @@
+package com.as.mitt.javainterview;
+
+public class DecimalToBinary {
+
+	public void converToBinary(int number) {
+		int [] binary= new int [25];
+		int index = 0;
+		while (number > 0) {
+			binary[index++] = number %2;
+			number = number /2;
+		}
+		for (int i=binary.length-1; i>= 0; i--) {
+			System.out.print(binary[i]);
+		}
+		
+	}
+	public static void main(String[] args) {
+		DecimalToBinary b = new DecimalToBinary();
+		b.converToBinary(8);
+	}
+
+	
+}
