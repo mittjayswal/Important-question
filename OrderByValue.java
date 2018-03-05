b71ea4421a695e35ca1c37407f82b4a7fea77f9f
mittjayswal <mittjayswal@gmail.com>
2018-03-04 20:24:57 -0800

    Sort Map by value using comparator

---
+++ OrderByValue.java
@@ -0,0 +1,37 @@
+package com.as.mitt.javainterview;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/*Sort or order a HashMap or TreeSet or any map item by value. Write a comparator
+which compares by value, not by key. Entry class might helps you here.*/
+
+public class OrderByValue {
+
+	public static void main(String[] args) {
+		
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Java", 1);
+		map.put("Unix", 8);
+		map.put("Spring", 2);
+		map.put("c", 10);
+		map.put("C#", 3);
+		
+		Set<Entry<String, Integer>> set = map.entrySet();
+		List<Entry<String, Integer>> list = new ArrayList<>(set);
+		
+		Collections.sort(list, (o1,o2) -> o1.getValue().compareTo(o2.getValue()));
+		
+		for (Entry<String, Integer> entry:list) {
+			System.out.println("key = "+ entry.getKey()+" value = "+entry.getValue());
+		}
+		
+	}
+	
+	
+}
