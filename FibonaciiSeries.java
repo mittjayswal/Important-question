24ea50d3c5a3693bb63630c7950c83e8527a86b9
mittjayswal <mittjayswal@gmail.com>
2018-03-08 18:09:18 -0800

    Fibonacii Series

---
+++ FibonaciiSeries.java
@@ -0,0 +1,25 @@
+package com.as.mitt.javainterview;
+
+public class FibonaciiSeries {
+
+	public static void main(String[] args) {
+		int number = 15;
+		
+		findSeries(number);
+		
+	}
+	public static void findSeries (int number){
+		int [] numberArray = new int[number];
+		numberArray[0] = 0;
+		numberArray[1] = 1;
+		for (int i=2; i<number; i++) {
+			numberArray[i] = numberArray[i-1] + numberArray[i-2];
+		}
+		System.out.print("Fibonacii series: ");
+		for (int i:numberArray) {
+			System.out.print(i+ " " );
+		}
+	}
+
+	
+}
