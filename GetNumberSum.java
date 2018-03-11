729cb5c6c0cdea4774188643dc6c0ebfefd2ad52
mittjayswal <mittjayswal@gmail.com>
2018-03-10 20:40:12 -0800

    Number Sum

---
+++ GetNumberSum.java
@@ -0,0 +1,32 @@
+package com.as.mitt.javainterview;
+
+/*Write a program to find sum of each digit in the given number using recursion.*/
+
+
+public class GetNumberSum {
+	
+	public static void main(String[] args) {
+		
+		GetNumberSum n1 = new GetNumberSum();
+		
+		int sum = n1.findNumberSum(223);
+		System.out.println(sum); 
+		
+	}
+
+	int sum =0;
+	
+	private int findNumberSum(int number) {
+		
+		if (number ==0) {
+			return number;
+		}else {
+			sum += number % 10;
+			findNumberSum(number/10);
+		}
+		return sum;
+		
+		
+	}
+	
+}
