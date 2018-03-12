c586142bf00a9f0dfe1c7de0fef47c11858bb33c
mittjayswal <mittjayswal@gmail.com>
2018-03-11 17:53:34 -0700

    String Permutation

---
+++ StringPermutationsEx.java
@@ -0,0 +1,44 @@
+package com.as.mitt.javainterview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringPermutationsEx {
+
+	 public static void main(String a[]) {
+		 
+	        List<String> output = StringPermutationsEx.generatePermutations("xyz");
+	        System.out.println("Result size: "+output.size());
+	        output.stream().forEach(System.out::println);
+	        System.out.println("------------------");
+	 
+	    }
+	 
+	    public static List<String> generatePermutations(String input) {
+	 
+	        List<String> strList = new ArrayList<String>();
+	        StringPermutationsEx.permutations("", input, strList);
+	 
+	        return strList;
+	    }
+	 
+	    private static void permutations(String consChars, String input, List<String> opContainer) {
+	 
+	        if(input.isEmpty()) {
+	        	//System.out.println("consChars+input"+consChars+"+"+input);
+	            opContainer.add(consChars);
+	            return;
+	        }
+	 
+	        for(int i=0; i<input.length(); i++) {
+	        	/*System.out.println("consChars+input.charAt(i)"+consChars+"---"+input.charAt(i));
+	        	System.out.println("substring and second substring = "+ input.substring(0, i)+"---"+input.substring(i+1));
+	        	System.out.println("opcontainer"+opContainer);
+	        	System.out.println("---------------------------------------"+i);*/
+	            permutations(consChars+input.charAt(i),
+	                            input.substring(0, i)+input.substring(i+1),
+	                            opContainer);
+	           // System.out.println("return");
+	        }
+	    }
+}
