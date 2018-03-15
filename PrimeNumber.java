877305b127fc94e423d1ab977de3d81ea0b4bb45
mittjayswal <mittjayswal@gmail.com>
2018-03-14 21:05:58 -0700

    Prime Number or not

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+package com.as.mitt.javainterview;
+
+public class PrimeNumber {
+
+	public static boolean findPrime(int number) {
+		for (int i = 2; i < number / 2; i++) {
+			if (number % 2 == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("17 Number is Prime = " + findPrime(17));
+		System.out.println("2 Number is Prime = " + findPrime(2));
+		System.out.println("6 Number is Prime = " + findPrime(6));
+	}
+}
