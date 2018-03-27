5435d1df010e542a10074513132b6bfc97958de2
mittjayswal <mittjayswal@gmail.com>
2018-03-27 14:01:35 -0700

    Social Security

--- CheckFID.java
+++ CheckFID.java
@@ -4,7 +4,44 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+/*JAVA PROGRAMMING PROBLEM
+-------------------------------------------------------
+Solve the problems using the Java programming language. 
+Choose your preferred way of testing these programs, either by using simple main method or by using JUnit test cases.  In either case, include the source code for the testing code you write.
+________________________________________________________________________
+Check if given string contains a valid Federal Identification (FID) number.  
+If it is not valid, return "invalid". 
+If it is valid, convert the string to one of two formats, depending on the value of the input  parameter EntityType :
+ 
+i)                    If the EntityType value is “P” (Personal), convert the string to "XXX-XX-XXXX" format.  
+ii)                   If the EntityType value is “B” (Business), convert the string to "XX-XXXXXXX" format.  
+ 
+You can assume the EntityType is set correctly by the user to either “P” or “B.”
+ 
+Valid input formats when EntityType = “P” are:
+123-44-9876   (“dashes” in the correct position)
+123 44 9876	(spaces in the correct position)
+123449876    (No spaces)
 
+You solution must also verify that only valid numbers/characters are entered.
+ 
+If the user inputs any other format than the above 3, the program should return “Invalid entry.”  
+For example, “123-44 9876”  is invalid because it uses one “-“ and one space.
+ 
+Valid input formats when EntityType = “B” are:
+98-1234567	(dash in the correct position for a business FID)
+981234567  	(no dashes)
+98 1234567 	(space in the correct position for a business FID)
+
+
+ALSO VALID ARE ANY VALID Personal FID’s, for example:
+981-23-4567	(this is because sometimes people enter the same format at personal without knowing the FID is broken up differently.)
+ 
+Examples
+222-44-9876  and EntityType=”P”                                         	222-44-9876
+123-44-5678  and EntityType=”B”                                         	12-34456789
+ABC-44-5678  and EntityType=”B”                         	Invalid Entry
+ */
 public class CheckFID {
 
 	public static final int FIDSIZE = 11;
